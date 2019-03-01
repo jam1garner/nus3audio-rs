b6f9f05cfb44bc06bf6d1b5dee44a328d2e7ef9f
jam1garner <jam1.mcleod@hotmail.com>
2019-03-01 17:03:04 -0500

    Add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+extern crate clap;
+use clap::{Arg, App, SubCommand};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = 
+        App::new("nus3audio")
+        .version("1.0")
+        .about("Tool for working with nus3audio files")
+        .author("jam1garner")
+        .arg(Arg::with_name("replace")
+                .help("Replaces a file at INDEX with NEWFILE")
+                .short("r")
+                .long("replace")
+                .value_names(&["INDEX", "NEWFILE"]))
+        .arg(Arg::with_name("write")
+                .help("Write to FILE after performing all other operations")
+                .short("w")
+                .value_name("FILE")
+                .takes_value(true)
+                .long("write"))
+        .arg(Arg::with_name("extract")
+                .help("Extract nus3audio contents to FOLDER")
+                .short("o")
+                .long("output")
+                .value_name("FOLDER")
+                .takes_value(true))
+        .arg(Arg::with_name("delete")
+                .help("Delete file at INDEX in nus3audio file")
+                .short("d")
+                .long("delete")
+                .value_name("INDEX")
+                .takes_value(true))
+        .arg(Arg::with_name("print")
+                .help("Prints the contents of the nus3audio file")
+                .short("p")
+                .long("print"))
+        .arg(Arg::with_name("visual")
+                .help("Edit in visual mode")
+                .short("v")
+                .long("visual"))
+        .arg(Arg::with_name("file")
+                .help("nus3audio file to open")
+                .required(true))
+        .get_matches();
 }
